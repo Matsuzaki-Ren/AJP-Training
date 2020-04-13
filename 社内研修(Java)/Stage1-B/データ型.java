12a55aa02e9f991fe990ecb17aeaa1b04061732d
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-04-13 14:29:25 +0900

    初回提出

---
+++ 社内研修(Java)/Stage1-B/データ型.java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main (String args[]){
+        char c = 'あ';
+        String str = "松﨑";
+        int number = 10;
+        boolean bool = 10 / 2 == 5;
+        String date = "4月13日";
+        System.out.println("文字型" + c);
+        System.out.println("文字列型" + str);
+        System.out.println("整数型" + number);
+        System.out.println("論理型" + bool); 
+        System.out.println("日付型" + date);
+    }
+}
