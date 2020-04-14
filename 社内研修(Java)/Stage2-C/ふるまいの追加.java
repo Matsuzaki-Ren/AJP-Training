40adc39d16e5eecbf1f5dbcd5b409c6f9d336b74
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-04-14 13:50:29 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-C/ふるまいの追加.java
@@ -0,0 +1,17 @@
+import java. util. Scanner;
+class hello4 {
+    public static void main (String args[]){
+        Scanner scanner =new Scanner(System.in);
+        String str;
+        str = "おつかれさまです";
+        String str2 = str. substring(0,1);
+        String str3 = str. substring(7,8);
+        System.out.println("入力された値は{" + str + "}です");
+        System.out.println("桁数{" +str.length() + "}" );
+        System.out.println("頭文字：{" +str2+ "}");
+        System.out.println("語尾：{" +str3+ "}");
+         if (str. contains("A"))
+        System.out.println("文字列にAを含む：" + "{No}");
+           System.out.println("文字列にAを含む：" + "{No}");
+    }
+}
