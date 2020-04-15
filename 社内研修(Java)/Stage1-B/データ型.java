5c9989a43c81a88a96cbfe5a59ff8d69cd5f94c8
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-04-15 14:44:28 +0900

    5回目提出

--- 社内研修(Java)/Stage1-B/データ型.java
+++ 社内研修(Java)/Stage1-B/データ型.java
@@ -5,10 +5,10 @@ public class Hello {
         int number = 10;
         boolean bool = 10 / 2 == 5;
         String date = "4月13日";
-        System.out.println("文字型" + c);
-        System.out.println("文字列型" + str);
-        System.out.println("整数型" + number);
-        System.out.println("論理型" + bool); 
-        System.out.println("日付型" + date);
+        System.out.println("文字型:" + c);
+        System.out.println("文字列型:" + str);
+        System.out.println("整数型:" + number);
+        System.out.println("論理型:" + bool); 
+        System.out.println("日付型:" + date);
     }
 }
