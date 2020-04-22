24f99d3c38723850e9939979c7fab11fcc77be9f
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-04-22 14:41:39 +0900

    2回目提出

--- 社内研修(Java)/Stage2-C/ふるまいの追加.java
+++ 社内研修(Java)/Stage2-C/ふるまいの追加.java
@@ -1,17 +1,29 @@
-import java. util. Scanner;
-class hello4 {
-    public static void main (String args[]){
-        Scanner scanner =new Scanner(System.in);
-        String str;
-        str = "おつかれさまです";
-        String str2 = str. substring(0,1);
-        String str3 = str. substring(7,8);
-        System.out.println("入力された値は{" + str + "}です");
-        System.out.println("桁数{" +str.length() + "}" );
-        System.out.println("頭文字：{" +str2+ "}");
-        System.out.println("語尾：{" +str3+ "}");
-         if (str. contains("A"))
-        System.out.println("文字列にAを含む：" + "{No}");
-           System.out.println("文字列にAを含む：" + "{No}");
-    }
+import java.util.Scanner;
+
+class Hello4{
+  public static void main(String args[]){
+    // Scannerを宣言                     
+    Scanner scan = new Scanner(System.in);
+    String str = scan.next();
+    // 文字列１の出力
+    System.out.println("入力された値は{"+ str + "}です");
+    int i = scan.nextInt();
+    // 整数の出力
+    System.out.println("桁数：{"+ i + "}桁");
+    String initial = scan.next();
+    // 文字列２の出力
+    System.out.println("頭文字：{"+ initial + "}");
+    String last = scan.next();
+    // 文字列３の出力
+    System.out.println("語尾：{"+ last + "}");
+    //指定した文字列が存在するか確認
+        if (str.contains("A"))
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+  }
 }
