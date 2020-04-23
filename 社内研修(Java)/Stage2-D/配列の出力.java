d13ff9a1326568babef84849846615e0c9ff1ce5
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-04-23 15:56:15 +0900

    2回目提出

--- 社内研修(Java)/Stage2-D/配列の出力.java
+++ 社内研修(Java)/Stage2-D/配列の出力.java
@@ -1,9 +1,13 @@
-public class Hello55 {
+public class Hello5_2 {
     public static void main(String[] args){
+        
+        //文字列Sを宣言する
         String S = "orange/apple/cherry/grape/apple";
+        //文字列SをHに変換する
         String H[] = S.split("/");
-        for(int i = 1; i < H.length; i++){
-        System.out.println(i + ":" + H[i]);
+        //for文を用いて出力する文字列の長さを決める
+        for(int i = 0; i < H.length; i++){
+        System.out.println(i + 1 + ":" + H[i]);
         }
     }
 }
