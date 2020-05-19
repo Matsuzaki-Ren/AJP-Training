8723b8550f5ab43349161bc7ccad08b4399b2222
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-05-19 16:22:01 +0900

    4回目提出

--- 社内研修(Java)/Stage2-E/重複の排除.java
+++ 社内研修(Java)/Stage2-E/重複の排除.java
@@ -1,13 +1,16 @@
 import java. util.*;
-public class Hello7{
+public class Stage2E{
     public static void main(String[] args){
-        //文字列変数fruitsを宣言。
+        //文字列変数fruitsを宣言
         String fruits = "apple/orrange/apple/grape/orrange";
-        //splitを使用し分割する。
+        //配列変数fruitsnameに変換
         String[] fruitsname = fruits.split("/", 0);
         List<String> fruitsList = new ArrayList<>();
+        //文字列の長さを取得する
          for (int i = 0; fruitsname.length > i ; i++){
+             //文字列の中にfruitsnameの文字が存在するかを確認(文字列の1番目と同じもの)
              if (!fruitsList.contains(fruitsname[i])){
+                 //文字列の中にfruitsnameの文字が存在するかを確認(文字列の2番目と同じもの)
                  for (int n = i + 1; fruitsname.length > n; n++){
                      if (fruitsname[i].equals(fruitsname[n])){
                          fruitsList.add(fruitsname[i]);
