38f6ed65a9b5abbc7c3055bf35825e27a429bc87
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-05-19 14:53:16 +0900

    4回目提出

--- 社内研修(Java)/Stage2-B/scanner出力.java
+++ 社内研修(Java)/Stage2-B/scanner出力.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
-class Hello3{
+class Stage2B{
   public static void main(String args[]){
     // Scannerを宣言                     
     Scanner scan = new Scanner(System.in);
     String str = scan.next();
-    // 文字列１の出力
-    System.out.println("入力された値は{"+ str + "}です");
-    int i = scan.nextInt();
-    // 整数の出力
-    System.out.println("桁数：{"+ i + "}桁");
-    String initial = scan.next();
-    // 文字列２の出力
-    System.out.println("頭文字：{"+ initial + "}");
-    String last = scan.next();
-    // 文字列３の出力
-    System.out.println("語尾：{"+ last + "}");
+  
+    
+    System.out.println("入力された値は{" + str + "}です");
+   
+   //文字列の長さを取得
+    int len = str.length();
+    
+    System.out.println("桁数:{" + len + "}");
+    
+    //文字列の頭文字を取得  
+     System.out.println("頭文字:{" + str.substring(0) + "}");
+    //文字列の語尾を取得
+     System.out.println("語尾;{" + str.substring(str.length()-1) + "}");
+  
   }
 }
