b3edb7ac93097613eed293eb239a0c9625e69c07
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-05-19 16:14:46 +0900

    2回目提出

--- 社内研修(Java)/Stage2-F/List変数の宣言.java
+++ 社内研修(Java)/Stage2-F/List変数の宣言.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-public class Hello8{
+public class Stage2F{
     public static void main(String[] args){
         List<String> week = new ArrayList<>();
        
@@ -12,8 +12,7 @@ public class Hello8{
         week.add("friday");
         week.add("saturday");
         week.add("sunday");
-        for(Iterator it = week.iterator(); it.hasNext();)
-        System.out.println(it.next());
+        System.out.println(week);
         
     }
 }
