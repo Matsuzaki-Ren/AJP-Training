13f803935094012fbe1b12808c894a74b20bfe13
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-06-01 16:41:41 +0900

    初回提出

---
+++ 社内研修(SQL)/Stage1-F/テーブル作成.sql
@@ -0,0 +1,110 @@
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '1001',
+   '大阪営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '2001',
+   '福岡営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '3001',
+   '広島営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '4001',
+   '京都営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '5001',
+   '和歌山営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '6001',
+   '静岡営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '7001',
+   '福井営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '8001',
+   '東京営業所',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '901',
+   '沖縄支部',
+   '0'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '901',
+   '沖縄支部',
+   '1'
+);
+INSERT INTO M_OFFICE (
+  OFFICE_CD,
+  SALES_OFFICE_NM,
+  DELET_FLG
+)
+VALUES
+(  '901',
+   '沖縄支部',
+   '1'
+);
