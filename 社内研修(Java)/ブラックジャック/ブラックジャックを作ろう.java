52d4ac6b3f72b321b818d918517377b1dbd118c9
Matsuzaki-Ren <63088745+Matsuzaki-Ren@users.noreply.github.com>
2020-07-06 14:50:56 +0900

    2回目提出

--- 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
+++ 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
@@ -4,55 +4,64 @@ public class BlackJack {
    //最初に出力されるランダム数値
    int first_number;
    first_number = (int)(Math.random()*10)+1;
-   //子数値
-   int yes_number;
-   yes_number = (int)(Math.random()*10)+1;
-   //子合計
-   int child_sum;
-   child_sum = first_number + yes_number;
-   //親数値
-   int no_number;
-   no_number = (int)(Math.random()*9)+14;
-   //親合計
-   int parent_sum;
-   parent_sum = no_number + child_sum;
-   //最初に出力されるランダム数値の出力
-   System.out.println("数値：[" + first_number + "]" + "合計【" + first_number + "】");
+   
+   
+      //子合計
+         int child_sum;
+         child_sum = first_number;
 
+   //最初に出力されるランダム数値の出力
+   System.out.println("数値：[" + first_number + "]" + "合計【" + child_sum + "】");
+   
     while(true){
         
-        //子数値
-      yes_number = (int)(Math.random()*10)+1;
-        
-      Scanner sc = new Scanner(System.in);
-        System.out.print("次のランダム数値を取得しますか[Y/N]");
-        String input = sc.nextLine();
+         boolean is =  true;
+          
+         Scanner sc = new Scanner(System.in);
+         System.out.print("『次のランダム数値を取得しますか[Y/N]』");
+         String input = sc.nextLine();
     
         switch (input){
             
             case "Y":
+                //子数値
+                int yes_number;
+                yes_number = (int)(Math.random()*10)+1;
+
+                child_sum += yes_number;
+                
                 System.out.println("数値：[" + yes_number + "]" + "合計【" + child_sum + "】");
-                if(yes_number > 21){
+                if(child_sum > 21){
+                    
                    System.out.println("LOSE");
-                   break; 
+                      is = false;
                 }
+                  break; 
             
             case "N":
+                
+                 //親数値
+                 int no_number;
                  no_number = (int)(Math.random()*9)+14;
-                 System.out.println("親数値：[" + parent_sum + "]" + "子合計【" + child_sum + "】");
+                 System.out.println("親数値：[" + no_number + "]" + "合計【" + child_sum + "】");
                  if(no_number > 22 || no_number < child_sum ){
                      System.out.println("WIN");
-                     break; 
-                 }if(no_number > child_sum){
-                     System.out.println("LOSE");
-                     break; 
-                 }if(no_number == child_sum){
-                     System.out.println("DRAW");
-                     break; 
+                        is = false;
                  }
-                 
-            default:
-        }
+                 if(no_number > child_sum){
+                     System.out.println("LOSE");
+                         is = false;
+                 }
+                 if(no_number == child_sum){
+                     System.out.println("DRAW");
+                      is = false;
+                 }
+                       break; 
+        } 
+         if(is == false){
+              is = false;
+              break;
+      }
     }
    }
 }
